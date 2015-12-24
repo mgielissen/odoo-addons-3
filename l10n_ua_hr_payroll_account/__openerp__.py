6d05ee675324c56df65ed7caeb8bdf1c9479a24d
Bogdan Lisnenko <lem8r.ka11a@gmail.com>
2015-12-24 14:27:34 +0200

    [IMP]payroll: accounting moves

--- l10n_ua_hr_payroll_account/__openerp__.py
+++ l10n_ua_hr_payroll_account/__openerp__.py
@@ -2,18 +2,18 @@
 {
     'name': 'Ukrainian - Payroll with Accounting',
     'category': 'Localization',
-    'depends': ['l10n_be_hr_payroll', 'hr_payroll_account', 'l10n_be'],
+    'depends': ['l10n_ua_hr_payroll', 'hr_payroll_account', 'l10n_ua'],
     'version': '1.0',
     'description': """
-Accounting Data for Belgian Payroll Rules.
-==========================================
+Бухгалтерські проведення для зарплати (МСФЗ)
+==============================================
     """,
 
     'auto_install': True,
-    'website': 'https://www.odoo.com/page/accounting',
+    # 'website': 'https://www.odoo.com/page/accounting',
     'demo': [],
     'data': [
-        # 'l10n_ua_hr_payroll_account_data.xml',
+        'l10n_ua_hr_payroll_account_data.xml',
     ],
     # 'post_init_hook': '_set_accounts',
     'installable': True
