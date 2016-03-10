ba166199e9e2d36a0f80f3eb1b887253f0b9c23e
Bogdan Lisnenko <lem8r.ka11a@gmail.com>
2016-03-10 18:31:49 +0200

    [IMP]theme_weche: basic colors defined

---
+++ theme_weche/__openerp__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Weche theme',
+    'description': 'Theme for weche.info site.',
+    'version': '1.0',
+    'author': 'Bohdan Lisnenko',
+
+    'data': [
+        'views/templates.xml'
+    ],
+    'depends': ['website'],
+}
